995045bb1b4e81a7be6a176af61163ee29b907ae
Christopher Whelan <whelan@ugcs.caltech.edu>
2012-11-11 19:29:00 -0800

    Add figsize to plot_series and clean up docstrings.

--- pandas/tools/plotting.py
+++ pandas/tools/plotting.py
@@ -1306,9 +1306,10 @@ def plot_frame(frame=None, x=None, y=None, subplots=False, sharex=True,
     ax : matplotlib axis object, default None
     style : list or dict
         matplotlib line style per column
-    kind : {'line', 'bar', 'barh'}
+    kind : {'line', 'bar', 'barh', 'kde'}
         bar : vertical bar plot
         barh : horizontal bar plot
+        kde : kernel density estimation plot
     logy : boolean, default False
         For line plots, use log scaling on y axis
     xticks : sequence
@@ -1371,26 +1372,25 @@ def plot_frame(frame=None, x=None, y=None, subplots=False, sharex=True,
 def plot_series(series, label=None, kind='line', use_index=True, rot=None,
                 xticks=None, yticks=None, xlim=None, ylim=None,
                 ax=None, style=None, grid=None, legend=False, logy=False,
-                secondary_y=False, **kwds):
+                secondary_y=False, figsize=None, **kwds):
     """
     Plot the input series with the index on the x-axis using matplotlib
 
     Parameters
     ----------
     label : label argument to provide to plot
-    kind : {'line', 'bar', 'barh'}
+    kind : {'line', 'bar', 'barh', 'kde'}
         bar : vertical bar plot
         barh : horizontal bar plot
-    rot : int, default 30
-        Rotation for tick labels
+        kde : kernel density estimation plot
+    rot : int, default None
+        Rotation for ticks
     use_index : boolean, default True
         Plot index as axis tick labels
     ax : matplotlib axis object
         If not passed, uses gca()
     style : string, default matplotlib default
         matplotlib line style to use
-    ax : matplotlib axis object
-        If not passed, uses gca()
     logy : boolean, default False
         For line plots, use log scaling on y axis
     xticks : sequence
@@ -1399,8 +1399,7 @@ def plot_series(series, label=None, kind='line', use_index=True, rot=None,
         Values to use for the yticks
     xlim : 2-tuple/list
     ylim : 2-tuple/list
-    rot : int, default None
-        Rotation for ticks
+    figsize : a tuple (width, height) in inches
     kwds : keywords
         Options to pass to matplotlib plotting method
 
@@ -1435,7 +1434,7 @@ def plot_series(series, label=None, kind='line', use_index=True, rot=None,
                      ax=ax, use_index=use_index, style=style,
                      xticks=xticks, yticks=yticks, xlim=xlim, ylim=ylim,
                      legend=legend, grid=grid, label=label,
-                     secondary_y=secondary_y, **kwds)
+                     secondary_y=secondary_y, figsize=figsize, **kwds)
 
     plot_obj.generate()
     plot_obj.draw()
